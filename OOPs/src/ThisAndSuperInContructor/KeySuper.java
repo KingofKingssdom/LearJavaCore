b24c5625ec38b14b003bfb5a273ccd913c67fe20
KingofKingssdom <duchuyd101@gmail.com>
2025-06-11 12:27:37 +0700

    update

--- OOPs/src/ThisAndSuperInContructor/KeySuper.java
+++ OOPs/src/ThisAndSuperInContructor/KeySuper.java
@@ -1,5 +1,55 @@
 package ThisAndSuperInContructor;
 
 public class KeySuper {
+	// Có 3 cách sử dụng
+	
+	// Gọi trực tiếp hàm dựng (constructor) của lớp cha gần nhất
+	// Ví dụ
+	/*
+	 * public class Parent {
+     
+    private String website;
+     
+    public Parent() {
+        System.out.println("This is parent");
+        print();
+    }
+     
+    public Parent(String website) {
+        this.website = website;
+        System.out.println("This is parent");
+        print();
+    }
+     
+    public void print() {
+        System.out.println("Website = " + website);
+    }
+}
+	 * 
+	 * 
+	 * public class Child extends Parent {
+ 
+    public Child() {
+        System.out.println("This is child");
+    }
+ 
+    public Child(String website) {
+        super(website);
+        System.out.println("This is child");
+    }
+     
+    public static void main(String[] args) {
+        Child child1 = new Child();
+        System.out.println("---");
+        Child child2 = new Child("gpcoder.com");
+    }
+}
 
+ // 2. GỌi đến biến của lớp Cha gần nhất
+  *  super.bienLopCha
+  
+  // 3. GỌi đến phương thức của lớp cha
+   * 
+   *  super.methodParent();
+	 * */
 }
--- OOPs/src/ThisAndSuperInContructor/KeyThis.java
+++ OOPs/src/ThisAndSuperInContructor/KeyThis.java
@@ -1,5 +1,65 @@
 package ThisAndSuperInContructor;
 
-public class KeyThis {
+import java.security.PublicKey;
 
+public class KeyThis {
+	// Từ khóa this có 6 cách sử dụng sau:
+	public static void main(String[] args) {
+		// 1. Từ khóa this trong java có thể được dùng để tham chiếu tới biến instance của lớp hiện tại.
+		// 2. Tham chiếu đến phường thức của lớp hiện tại, nếu không có từ khóa this, trình biên dịch sẽ từ thêm
+		// Ví dụ 
+		//  private int id;
+		/*
+		 * public void print() {
+        		System.out.println("Id = " + this.id);
+        		this.printWebsite();
+        		this.printSubject();
+    		}
+		 * */
+		
+		// 3. Gọi đến contructor của lớp hiện tại 
+		// Ví dụ
+		/*
+		 * public UsingThisExample(String website ) {
+        this(); // Bắt buộc phải dòng lệnh đầu tiên trong constructor
+        this.website = website ;
+    	}
+ 
+    	public UsingThisExample(String website, String subject) {
+        this(website); // Bắt buộc phải dòng lệnh đầu tiên trong constructor
+        this.subject = subject;
+    	}
+		 * */ 
+		// 4. Tham chiếu đến biến instance của lớp hiện tại 
+		// Ví dụ trong hàm contructor có 
+		// public Contructor(String name){
+		// this.name = name   => name ở biến this sẽ là tham chiếu ở trên xuống
+		
+		// 5. Sử dụng là làm tham số cho một phương thức 
+		/*
+		 * public class Helper {
+     
+    		public void print(UsingThisExample ex) {
+        		System.out.println("Id = " + ex.getId());
+        		System.out.println("Website = " + ex.getWebsite());
+    		}
+    		
+    	public class UsingThisExample {
+    		private int id;
+    		private String website;
+     
+		    UsingThisExample () {
+		        this.id = 1;
+		        this.website = "gpcoder";
+		    }
+     
+		    public void print() {
+		        Helper helper = new Helper();
+		        helper.print(this); 
+		    }
+     	}
+		 * */
+		
+		// 6. Tương tự như phương thức cũng có thể truyền làm tham số cho contructor
+	}
 }
